598456c5de125c0562eb52204bce432ec18ed3a0
Ahmet Küçükaydıner <ahmetkucukaydiner@gmail.com>
2023-08-02 23:42:48 +0300

    admin layouts added.

---
+++ Traversal.Business/Concrete/GuideManager.cs
@@ -0,0 +1,41 @@
+﻿using Traversal.Business.Abstract;
+using Traversal.DataAccess.Abstract;
+using Traversal.Entities.Concrete;
+
+namespace Traversal.Business.Concrete
+{
+    public class GuideManager : IGuideService
+    {
+        private IGuideDal _guideDal;
+
+        public GuideManager(IGuideDal guideDal)
+        {
+            _guideDal = guideDal;
+        }
+
+        public void TAdd(Guide entity)
+        {
+            _guideDal.Add(entity);
+        }
+
+        public void TDelete(Guide entity)
+        {
+            _guideDal.Delete(entity);
+        }
+
+        public void TUpdate(Guide entity)
+        {
+            _guideDal.Update(entity);
+        }
+
+        public List<Guide> TGetList()
+        {
+            return _guideDal.GetList();
+        }
+
+        public Guide TGetByID(int id)
+        {
+            return _guideDal.Get(x => x.Id == id);
+        }
+    }
+}
---
+++ TraversalCoreProje/ViewComponents/MemberDashboard/_GuideList.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Traversal.Business.Concrete;
+using Traversal.DataAccess.EntityFramework;
+
+namespace TraversalCoreProje.ViewComponents.MemberDashboard
+{
+    public class _GuideList : ViewComponent
+    {
+        private GuideManager guideManager = new GuideManager(new EfGuideDal());
+        public IViewComponentResult Invoke()
+        {
+            var values = guideManager.TGetList();
+            return View(values);
+        }
+    }
+}
