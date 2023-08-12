f0ae366d0fdaefc0cfe6c937ed30ad81fd12b279
AhmetKucukaydiner <ahmetkucukaydiner@gmail.com>
2023-08-12 18:30:34 +0300

    AdminDashboard revize

---
+++ TraversalCoreProje/ViewComponents/AdminDashboard/_Cards1Statistics.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Traversal.DataAccess.Concrete;
+
+namespace TraversalCoreProje.ViewComponents.AdminDashboard
+{
+    public class _Cards1Statistics : ViewComponent
+    {
+        Context c = new Context();
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.TourCount = c.Destinations.Count();
+            ViewBag.CustomerCount = c.Users.Count();
+            return View();
+        }
+    }
+}
