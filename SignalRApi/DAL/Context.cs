6db9ce22764de81bc37204290eb7f261a2d84e52
Ahmet Küçükaydıner <ahmetkucukaydiner@gmail.com>
2023-08-28 22:17:55 +0300

    signal R and postgresql added

---
+++ SignalRApi/DAL/Context.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SignalRApi.DAL;
+
+namespace Traversal.SignalRApi.DAL
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Visitor> Visitors { get; set; }
+    }
+}
---
+++ SignalRApi/DAL/Visitor.cs
@@ -0,0 +1,20 @@
+﻿namespace SignalRApi.DAL
+{
+    public enum ECity
+    {
+        Edirne = 1,
+        İstanbul = 2,
+        Ankara = 3,
+        Balıkesir = 4,
+        Antalya = 5,
+        Bursa = 6
+    }
+
+    public class Visitor
+    {
+        public int ID { get; set; }
+        public ECity City { get; set; }
+        public int CityVisitCount { get; set; }
+        public DateTime VisitDate { get; set; }
+    }
+}
