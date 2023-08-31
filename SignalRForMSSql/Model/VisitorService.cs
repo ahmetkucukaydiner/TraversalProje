b7ca0e3093855d0d855a4d245f8b12ffb3e44f9c
Ahmet Küçükaydıner <ahmetkucukaydiner@gmail.com>
2023-08-31 22:51:52 +0300

    RoleController eklendi. SignalR ile canlı data tablosu oluşturuldu.

--- SignalRForMSSql/Model/VisitorService.cs
+++ SignalRForMSSql/Model/VisitorService.cs
@@ -26,7 +26,7 @@ namespace Traversal.SignalRApiForMSSql.Model
         {
             await _context.Visitors.AddAsync(visitor);
             await _context.SaveChangesAsync();
-            await _hubContext.Clients.All.SendAsync("CallVisitorList", "aaaaaa");
+            await _hubContext.Clients.All.SendAsync("ReceiveVisitorList", GetVisitorChartList());
         }
 
         public List<VisitorChart> GetVisitorChartList()
@@ -34,7 +34,7 @@ namespace Traversal.SignalRApiForMSSql.Model
             List<VisitorChart> visitorCharts = new List<VisitorChart>();
             using (var command = _context.Database.GetDbConnection().CreateCommand())
             {
-                command.CommandText = "query";
+                command.CommandText = "select tarih,[1],[2],[3],[4],[5],[6] from (select [City], CityVisitCount, CAST([VisitDate] as Date) as tarih from Visitors) as visitTable pivot (Sum(CityVisitCount) For City in ([1],[2],[3],[4],[5],[6])) as pivottable order by tarih asc";
                 command.CommandType = CommandType.Text;
                 _context.Database.OpenConnection();
                 using (var reader = command.ExecuteReader())
@@ -45,7 +45,15 @@ namespace Traversal.SignalRApiForMSSql.Model
                         visitorChart.VisitDate = reader.GetDateTime(0).ToShortDateString();
                         Enumerable.Range(1, 6).ToList().ForEach(x =>
                         {
-                            visitorChart.Counts.Add(reader.GetInt32(x));
+                            if (DBNull.Value.Equals(reader[x]))
+                            {
+                                visitorChart.Counts.Add(0);
+                            }
+                            else
+                            {
+                                visitorChart.Counts.Add(reader.GetInt32(x));
+                            }
+
                         });
                         visitorCharts.Add(visitorChart);
                     }
--- TraversalCoreProje/Program.cs
+++ TraversalCoreProje/Program.cs
@@ -54,6 +54,11 @@ builder.Services.AddMvc(config =>
 
 builder.Services.AddMvc();
 
+builder.Services.ConfigureApplicationCookie(options =>
+{
+    options.LoginPath = "/Login/SignIn/";
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -88,14 +93,6 @@ app.UseEndpoints(endpoints =>
         name: "areas",
         pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
 
-});
-
-app.UseEndpoints(endpoints =>
-{
-    endpoints.MapControllerRoute(
-        name: "areas",
-        pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
-    );
 });
 
 app.Run();
