83e8b7a73f2a7df492f7c94b92b45fa6be42e239
Ahmet Küçükaydıner <ahmetkucukaydiner@gmail.com>
2023-08-31 23:20:25 +0300

    user role list.

---
+++ TraversalCoreProje/Areas/Admin/Models/RoleAssignViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace TraversalCoreProje.Areas.Admin.Models
+{
+    public class RoleAssignViewModel
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public bool RoleExist { get; set; }
+    }
+}
