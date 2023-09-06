dc26716e85899af56c8b5476c00c70bbc4dcb4ea
AhmetKucukaydiner <ahmetkucukaydiner@gmail.com>
2023-09-06 21:01:04 +0300

    forget password and reset password added.

---
+++ TraversalCoreProje/Controllers/PasswordChangeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProje.Controllers
+{
+    public class PasswordChangeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
